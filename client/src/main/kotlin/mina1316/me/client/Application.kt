27cd859424c974306ff79a01dd367f91cc28220c
Mina-1316 <mina1316.canreply@gmail.com>
2023-02-14 21:21:26 +0900

    reset client file for project branch isolation

--- client/src/main/kotlin/mina1316/me/client/Application.kt
+++ client/src/main/kotlin/mina1316/me/client/Application.kt
@@ -1,7 +1,5 @@
 package mina1316.me.client
 
-import mina1316.me.client.config.storage.StorageManager
-import mina1316.me.client.storage.Configuration
 import javax.swing.JFrame
 
 class Application() : JFrame() {
@@ -11,28 +9,4 @@ class Application() : JFrame() {
         setSize(300, 300)
         isVisible = true
     }
-}
-
-fun main(args: Array<String>) {
-//    val logger = SimpleLogger(
-//        Application::class.simpleName,
-//        Level.DEBUG,
-//        false,
-//        true,
-//        true,
-//        true,
-//        "yyyy-MM-dd HH:mm:ss.SSS",
-//        null,
-//        null,
-//        System.out
-//    )
-//
-//
-//    logger.info("Starting application")
-
-    System.setProperty("client.storage.name", "client.mvstore")
-
-    StorageManager(Configuration::class)
-    Application()
-    println("Hello, world!")
 }
--- client/src/main/kotlin/mina1316/me/client/storage/SessionStorage.kt
+++
@@ -1,7 +0,0 @@
-package mina1316.me.client.storage
-
-import mina1316.me.client.config.storage.annotation.Storage
-
-@Storage("session")
-class SessionStorage {
-}
